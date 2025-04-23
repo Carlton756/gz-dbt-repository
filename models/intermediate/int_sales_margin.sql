d97d36169193180a9877ec6fa96ee645bd723252
Carlton Francis <dymnfrncs@gmail.com>
2025-04-23 05:33:05 +0000

    commit int sale margin version 0

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,11 @@
 select 
-purchase_price*quantity AS purchase_cost,
-revenue - purchase_cost AS margin
+products_id,
+date_date,
+orders_id,
+revenue,
+quantity,
+ROUND(purchase_price*quantity, 2) AS purchase_cost,
+ROUND((revenue - ROUND(purchase_price*quantity, 2)), 2) AS margin
 
 FROM 
 {{ref("stg_gz_raw_data__raw_gz_product")}}
