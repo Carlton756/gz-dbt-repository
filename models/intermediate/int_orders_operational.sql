7180da4d1e474d78681650166b2eb2edb4e8cd7e
Carlton Francis <dymnfrncs@gmail.com>
2025-04-23 06:53:53 +0000

    commit

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,15 +1,15 @@
 select
-    orders_id
-     ,date_date
-     ,ROUND(margin + shipping_fee - (logcost + ship_cost),2) AS operational_margin
-     ,quantity
-     ,revenue
-     ,purchase_cost
-     ,margin
-     ,shipping_fee
-     ,logcost
-     ,ship_cost
+    orders_id,
+    date_date,
+    round((margin + shipping_fee - (logcost + ship_cost)), 2) as operational_margin,
+    quantity,
+    revenue,
+    purchase_cost,
+    margin,
+    shipping_fee,
+    logcost,
+    ship_cost
 
     {{ ref("int_orders_margin") }}
-join {{ ref("stg_gz_raw_data__raw_gz_ship") }} using (orders_id)
+left join {{ ref("stg_gz_raw_data__raw_gz_ship") }} using (orders_id)
 order by orders_id desc
